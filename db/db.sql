1feca56367b2744c421c8b7f917e726da7a690c9
Andrea <corraleshyulia@gmail.com>
2020-11-23 21:09:59 -0500

    database firstPart

--- db/db.sql
+++ db/db.sql
@@ -3,23 +3,35 @@ CREATE DATABASE databese_politiqueando;
 
 USE databese_politiqueando;
 
-CREATE TABLE usuarios(
+CREATE TABLE usuarios (
     usuario_id INT AUTO_INCREMENT PRIMARY KEY,
     usuario VARCHAR(16) NOT NULL,
     contrasenia VARCHAR(255) NOT NULL,
     nombre VARCHAR(100) NOT NULL,
     apellido VARCHAR(100) NOT NULL,
-    correo VARCHAR(100) NOT NULL,
-)
+    correo VARCHAR(100) NOT NULL
+);
 
 
-CREATE TABLE foros(
-    comentario VARCHAR(255) NOT NULL,
+CREATE TABLE comentarios(
+    usuario_id int,
+    descripcion TEXT NOT NULL,
+    creado timestamp NOT NULL DEFAULT current_timestamp,
     foreign key (usuario_id) references usuarios (usuario_id)
-)
+);
+
+CREATE TABLE reacciones(
+    usuario_id int,
+    reaccion VARCHAR(255) NOT NULL,
+    foreign key (usuario_id) references usuarios (usuario_id)
+);
+
 
 ALTER TABLE users
     ADD PRIMARY KEY (id);
 
 ALTER TABLE users
 MODIFY id INT(11) NOT NULL AUTO_INCREMENT;
+
+
+DESCRIBE usuarios
---
+++ db/keys.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+  database: {
+    connectionLimit: 10,
+    host: 'baqobyesfgdnf1oled8c-mysql.services.clever-cloud.com',
+    user: 'uq3b9r6nzeaxaahz',
+    password: 'kFAuUPPeJAAcQSBkTRLE',
+    database: 'baqobyesfgdnf1oled8c'
+  }
+}
